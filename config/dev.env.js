45e219d43d1711bd35fd564684d80727844afc94
sidney <sidney.hang.y@gmail.com>
2021-03-15 10:26:15 +0800

    first commit

---
+++ config/dev.env.js
@@ -0,0 +1,7 @@
+module.exports = {
+    NODE_ENV: '"development"',
+    BASE_API: '"https://appapi.weicai001.com"',
+    // BASE_API: '"http://testweb.weicai001.com"',
+    // BASE_API: '"http://47.103.16.240:8765"',
+    APP_ORIGIN: '"https://wallstreetcn.com"'
+}
---
+++ config/prod.env.js
@@ -0,0 +1,5 @@
+module.exports = {
+    NODE_ENV: '"production"',
+    BASE_API: '"https://appapi.weicai001.com"',
+    APP_ORIGIN: '"https://wallstreetcn.com"'
+};
